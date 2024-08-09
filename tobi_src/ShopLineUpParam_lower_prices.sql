41cb0c9b974dc043b54df8cba465902b1b6a7c98
White Goose <t.lauppe@outlook.com>
2024-08-09 20:02:22 +0200

    Create ShopLineUpParam_lower_prices.sql

---
+++ tobi_src/ShopLineUpParam_lower_prices.sql
@@ -0,0 +1,23 @@
+SELECT
+  a.ID,
+  a.Name,
+  a.equipId,
+  (a.value.toString().slice(0, -1)) AS value,
+  a.mtrlId,
+  a.eventFlag_forStock,
+  a.eventFlag_forRelease,
+  a.sellQuantity,
+  a.pad3,
+  a.equipType,
+  a.costType,
+  a.pad1,
+  a.setNum,
+  a.value_Add,
+  a.value_Magnification,
+  a.iconId,
+  a.nameMsgId,
+  a.menuTitleMsgId,
+  a.menuIconId,
+  a.pad2
+WHERE
+  a.value.length > 1
